1cc07913b50a9926e6904b7623dd67b958db082e
yuzhe0918 <xiangyuzhe0918@126.com>
2014-03-18 17:04:41 +0800

    显示信息。

--- repast/view/repast.py
+++ repast/view/repast.py
@@ -4,5 +4,10 @@ from flask import request, render_template
 
 def to_repast_by_stores_id(stores_id):
     '''根据id到餐厅页面'''
+    if stores_id == 1:
+        message = '喵喵餐厅'
+    if stores_id == 2:
+        message = '饭饭餐厅'
     return render_template('repast.html',
-                           stores_id=stores_id)
+                           stores_id=stores_id,
+                           message=message)
