887f508b0bf96eb33fc1024a85055d7f63432fff
yuzhe0918 <xiangyuzhe0918@126.com>
2014-03-24 16:48:26 +0800

    测试分享。

--- repast/static/javascript/bottom.js
+++ repast/static/javascript/bottom.js
@@ -1,9 +1,19 @@
 /**
  * Created by K on 3/24/14.
  */
-$(document).ready(function(){
+
    document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
        WeixinJSBridge.call('showToolbar');
     });
+    function weixinSendAppMessage(title,desc,link,imgUrl){
+        WeixinJSBridge.invoke('sendAppMessage',{
+        //"appid":appId,
+        "img_url":imgUrl,
+        //"img_width":"640",
+        //"img_height":"640",
+        "link":link,
+        "desc":desc,
+        "title":title
+        });
+    }
 
-});
