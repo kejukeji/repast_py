f4c61553a8124b485231d37be0135a0ce541ca8a
Huangxiaohen <huangxiaohen2738@gmail.com>
2014-06-03 15:12:54 +0800

    " Close to you"

--- repast/weixin/verify.py
+++ repast/weixin/verify.py
@@ -1,6 +1,4 @@
 # coding: utf-8
-
-
 from flask import request, make_response
 from xml.etree import ElementTree as ET
 from repast.weixin.tools import parse_request
@@ -25,6 +23,7 @@ def weixin():
             for q in queue:
                 user = User.query.filter(User.id == q.user_id).first()
                 loop_message(user.openid,web_chat)
+    num+=1
 
     if request.method == "GET":
         if web_chat.validate(**parse_request(request.args, ("timestamp", "nonce", "signature"))):
@@ -45,7 +44,7 @@ def weixin():
         if MsgType == 'location':
             return response_location(xml_recv, web_chat)
 
-    num+=1
+
 
 num=0
 
