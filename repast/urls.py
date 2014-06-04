7b9967d98145886910b902a4921594c8f0e0824c
yuzhe0918 <xiangyuzhe0918@126.com>
2014-06-04 10:19:42 +0800

    已选菜的隐藏显示

--- repast/urls.py
+++ repast/urls.py
@@ -28,7 +28,7 @@ from restfuls.queue import AjaxCallNumber
 from services.food_list_service import GetDishes
 from .view.order_dishes.my_coupons import *
 from .restfuls.coupons import *
-from .restfuls.dish import AddDish
+from .restfuls.dish import AddDish, ShowDish
 from .view.order_dishes.static import *
 from .view.order_dishes.a_la_carte import to_pay_1,to_pay_5
 
@@ -102,6 +102,9 @@ api.add_resource(AjaxCallNumber, '/restful/call_number/<int:shop_assistant_id>')
 api.add_resource(GetDishes,'/services/get_foods/<int:dish_sort_id>')
 api.add_resource(UpdateUserCoupons,'/restful/coupons_id/<int:coupons_id>')
 api.add_resource(AddDish, '/restful/add/dish')
+
+# 获得已选菜品
+api.add_resource(ShowDish, '/restful/get/choose/dish')
 
 
 # 后台管理路径
